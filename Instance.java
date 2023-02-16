41bbe1cb1f00ada9a0f18abd2e18cb35dfff2420
ch-28 <112527437+ch-28@users.noreply.github.com>
2023-02-16 02:16:27 -0600

    Add files via upload

---
+++ Instance.java
@@ -0,0 +1,104 @@
+package project1;
+
+public class Instance {
+	
+	private Object windy;
+	private Object outlook;
+	private Object temperature;
+	private Object humidity;
+	private Object play;
+	{
+		  Object temperature;
+		Object humidity;
+		Object play;
+		constructor(outlook, temperature, humidity, windy, play) {
+		    this.outlook = outlook;
+		    this.temperature = temperature;
+		    this.humidity = humidity;
+		    this.windy = windy;
+		    this.play = play;
+		  }
+		}
+	  class Dataset {
+		  private Class<byte[]> instances;
+
+		void constructor() {
+		    this.instances = [];
+		  }
+		}
+	  const dataset = new Dataset();
+	  
+	    Object results;
+		download: true,
+	    header: true,
+	    complete: function(results) {
+	    	Object data;
+			results.data.forEach(function(data) {
+	    }
+	        Instance instance = new Instance(data.outlook, data.temperature, data.humidity, data.windy, data.play);
+	        dataset.instances.push(instance);
+	      });
+	    }
+	private Object function(Object data) {
+		return null;
+	}
+	private void constructor(Object outlook2, Object temperature2, Object humidity2, Object windy2, Object play2) {
+		
+		
+	}
+	  });
+	  class Dataset1 {
+		  // ...
+
+		  @SuppressWarnings("hiding")
+		void (outlook, temperature, humidity, windy) {
+		    // Simple decision tree algorithm
+		    if (outlook == 'sunny') {
+		      if (humidity <= 75) {
+		        return 'yes';
+		      } else {
+		        return 'no';
+		      }
+		    } else if (outlook == 'overcast') {
+		      return 'yes';
+		    } else if (outlook === 'rainy') {
+		      if (windy === 'TRUE') {
+		        return 'no';
+		      } else {
+		        return 'yes';
+		      }
+		    }
+		  }
+		
+	  class Dataset {
+		  // ...
+
+		  void addInstance(outlook , temperature, humidity, windy, play) {
+		    const instance = new Instance(outlook, temperature, humidity, windy, play);
+		    this.instances.push(instance);
+		  }
+		}
+	  class Dataset {
+		  // ...
+
+		  changeActivity(instance, newPlay) {
+		    instance.play = newPlay;
+		  }
+		}
+
+
+
+}
+
+	private void constructor1(Object outlook2, Object temperature, Object humidity, Object windy2, Object play) {
+		
+		
+	}
+	private void constructor11(Object outlook2, Object temperature2, Object humidity2, Object windy2, Object play2) {
+		
+		
+	}
+	private void constructor(Object outlook2, Object temperature2, Object humidity2, Object windy2, Object play2) {
+		
+		
+	}
